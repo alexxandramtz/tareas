58a4abbaa91662fc5b45a8880ea8f864852580fa
alexxandramtz <jennyflamers@gmail.com>
2024-03-30 00:03:50 -0600

    Tarea06 FUNCION SIN RETURN

---
+++ TAREA06.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/*Haciendo uso de funciones quiero realizar una suma de dos números enteros*/
+/*y sacar el promedio de la suma de los dos números, esos numeros se los*/
+/*pediré al usuario, así como también le pediré su primer nombre nada más.*/
+/*Al final tengo que mostrar en consola, los números que el usuario ingreso, el*/
+/*promedio y su nombre*/
+
+
+int promediodesuma(int numero1, int numero2){
+	
+	return numero1 + numero2;
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+	int n1,n2,suma,promedio;
+	char nombre [20];
+	
+	printf("ingresa tu nombre:\n");
+	fgets(nombre,sizeof (nombre),stdin);
+	
+	
+	
+	
+	printf("Ingresa el primer numero entero:\n");
+	scanf("%d",&n1);
+	
+	printf("Ingresa el segundo numero entero:\n");
+	scanf("%d",&n2);
+	suma = n1+n2;
+	promedio=suma /2;
+	//promedioresultado =  (n1,n2)/2;
+	
+	printf("El resultado de la suma es: %d\n",suma);
+	printf("El promedio de la suma es: %d",promedio);
+	
+	return 0;
+}
+
